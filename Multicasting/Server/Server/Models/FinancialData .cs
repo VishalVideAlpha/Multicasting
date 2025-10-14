ac2075584882c8d4d9e242eb1b833cd5a70c75fd
unknown <Vishal@videalpha.com>
2025-10-14 10:42:24 +0530

    new

---
+++ Multicasting/Server/Server/Models/FinancialData .cs
@@ -0,0 +1,201 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class FinancialData : INotifyPropertyChanged
+{
+    private string _binding;
+    private string _branch;
+    private string _group;
+    private string _client;
+    private string _staffName;
+    private decimal _spanMargin;
+    private decimal _exposureMargin;
+    private decimal _totalMargin;
+    private decimal _peakMargin;
+    private decimal _exposure;
+    private decimal _varTotal;
+    private decimal _peakVar;
+    private decimal _todaysDelivery;
+    private decimal _totalDelivery;
+    private decimal _peakDeliv;
+    private decimal _optRealDay;
+    private decimal _optUnrealDay;
+    private decimal _optMtmNet;
+    private decimal _optMtmDay;
+    private decimal _futRealDay;
+    private decimal _futUnrealDay;
+    private decimal _futMtmNet;
+    private decimal _futMtmDay;
+    private decimal _allotedFund;
+    private double _utilizedPercentage;
+    private decimal _exposureMargin2;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public string Binding
+    {
+        get => _binding;
+        set => SetProperty(ref _binding, value);
+    }
+
+    public string Branch
+    {
+        get => _branch;
+        set => SetProperty(ref _branch, value);
+    }
+
+    public string Group
+    {
+        get => _group;
+        set => SetProperty(ref _group, value);
+    }
+
+    public string Client
+    {
+        get => _client;
+        set => SetProperty(ref _client, value);
+    }
+
+    public string StaffName
+    {
+        get => _staffName;
+        set => SetProperty(ref _staffName, value);
+    }
+
+    public decimal SpanMargin
+    {
+        get => _spanMargin;
+        set => SetProperty(ref _spanMargin, value);
+    }
+
+    public decimal ExposureMargin
+    {
+        get => _exposureMargin;
+        set => SetProperty(ref _exposureMargin, value);
+    }
+
+    public decimal TotalMargin
+    {
+        get => _totalMargin;
+        set => SetProperty(ref _totalMargin, value);
+    }
+
+    public decimal PeakMargin
+    {
+        get => _peakMargin;
+        set => SetProperty(ref _peakMargin, value);
+    }
+
+    public decimal Exposure
+    {
+        get => _exposure;
+        set => SetProperty(ref _exposure, value);
+    }
+
+    public decimal VarTotal
+    {
+        get => _varTotal;
+        set => SetProperty(ref _varTotal, value);
+    }
+
+    public decimal PeakVar
+    {
+        get => _peakVar;
+        set => SetProperty(ref _peakVar, value);
+    }
+
+    public decimal TodaysDelivery
+    {
+        get => _todaysDelivery;
+        set => SetProperty(ref _todaysDelivery, value);
+    }
+
+    public decimal TotalDelivery
+    {
+        get => _totalDelivery;
+        set => SetProperty(ref _totalDelivery, value);
+    }
+
+    public decimal PeakDeliv
+    {
+        get => _peakDeliv;
+        set => SetProperty(ref _peakDeliv, value);
+    }
+
+    public decimal OPTRealDay
+    {
+        get => _optRealDay;
+        set => SetProperty(ref _optRealDay, value);
+    }
+
+    public decimal OPTUnrealDay
+    {
+        get => _optUnrealDay;
+        set => SetProperty(ref _optUnrealDay, value);
+    }
+
+    public decimal OPTMTMNet
+    {
+        get => _optMtmNet;
+        set => SetProperty(ref _optMtmNet, value);
+    }
+
+    public decimal OPTMTMDay
+    {
+        get => _optMtmDay;
+        set => SetProperty(ref _optMtmDay, value);
+    }
+
+    public decimal FUTRealDay
+    {
+        get => _futRealDay;
+        set => SetProperty(ref _futRealDay, value);
+    }
+
+    public decimal FUTUnrealDay
+    {
+        get => _futUnrealDay;
+        set => SetProperty(ref _futUnrealDay, value);
+    }
+
+    public decimal FUTMTMNet
+    {
+        get => _futMtmNet;
+        set => SetProperty(ref _futMtmNet, value);
+    }
+
+    public decimal FUTMTMDay
+    {
+        get => _futMtmDay;
+        set => SetProperty(ref _futMtmDay, value);
+    }
+
+    public decimal AllotedFund
+    {
+        get => _allotedFund;
+        set => SetProperty(ref _allotedFund, value);
+    }
+
+    public double UtilizedPercentage
+    {
+        get => _utilizedPercentage;
+        set => SetProperty(ref _utilizedPercentage, value);
+    }
+
+    public decimal ExposureMargin2
+    {
+        get => _exposureMargin2;
+        set => SetProperty(ref _exposureMargin2, value);
+    }
+
+    protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (!Equals(field, value))
+        {
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Multicasting/Server/Server/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class UdpServer
+{
+    static Dictionary<string, IPEndPoint> clients = new Dictionary<string, IPEndPoint>();
+    static Dictionary<string, FinancialData> tradingData = new Dictionary<string, FinancialData>();
+    static UdpClient udpServer;
+
+    static void Main()
+    {
+        udpServer = new UdpClient(5505);
+        Console.WriteLine("UDP Server started on port 5505...");
+
+        Thread dataUpdateThread = new Thread(UpdateTradingData);
+        dataUpdateThread.Start();
+
+        while (true)
+        {
+            ReceiveClientData();
+        }
+    }
+
+    static void ReceiveClientData()
+    {
+        IPEndPoint clientEP = new IPEndPoint(IPAddress.Any, 0);
+        byte[] buffer = udpServer.Receive(ref clientEP);
+
+        string clientMessage = Encoding.ASCII.GetString(buffer).Trim();
+        Console.WriteLine($"Received data from {clientEP}: {clientMessage}");
+
+        if (clientMessage.StartsWith("ID:"))
+        {
+            string userId = clientMessage.Substring(3).Trim();
+            lock (clients)
+            {
+                if (!clients.ContainsKey(userId))
+                {
+                    clients[userId] = clientEP;
+                    Console.WriteLine($"Client registered with User ID: {userId}");
+                }
+            }
+            SendInitialData(clientEP);
+        }
+        else
+        {
+            // Process other types of client messages if necessary
+        }
+    }
+
+    static void SendInitialData(IPEndPoint clientEP)
+    {
+        string initialData = SerializeTradingData();
+        byte[] data = Encoding.ASCII.GetBytes(initialData);
+        udpServer.Send(data, data.Length, clientEP);
+    }
+
+    static void UpdateTradingData()
+    {
+        Random rand = new Random();
+        while (true)
+        {
+            lock (tradingData)
+            {
+                foreach (var key in tradingData.Keys.ToList())
+                {
+                    var financialData = tradingData[key];
+
+                    // Random updates for demonstration purposes. Replace with actual logic as needed.
+                    financialData.SpanMargin = (decimal)(rand.NextDouble() * 10000);
+                    financialData.ExposureMargin = (decimal)(rand.NextDouble() * 5000);
+                    financialData.TotalMargin = (decimal)(rand.NextDouble() * 15000);
+                    financialData.PeakMargin = (decimal)(rand.NextDouble() * 20000);
+                    financialData.Exposure = (decimal)(rand.NextDouble() * 1000000);
+                    financialData.VarTotal = (decimal)(rand.NextDouble() * 50000);
+                    financialData.PeakVar = (decimal)(rand.NextDouble() * 30000);
+                    financialData.TodaysDelivery = (decimal)(rand.NextDouble() * 1000);
+                    financialData.TotalDelivery = (decimal)(rand.NextDouble() * 5000);
+                    financialData.PeakDeliv = (decimal)(rand.NextDouble() * 10000);
+                    financialData.OPTRealDay = (decimal)(rand.NextDouble() * 500);
+                    financialData.OPTUnrealDay = (decimal)(rand.NextDouble() * 700);
+                    financialData.OPTMTMNet = (decimal)(rand.NextDouble() * 1000);
+                    financialData.OPTMTMDay = (decimal)(rand.NextDouble() * 1500);
+                    financialData.FUTRealDay = (decimal)(rand.NextDouble() * 200);
+                    financialData.FUTUnrealDay = (decimal)(rand.NextDouble() * 300);
+                    financialData.FUTMTMNet = (decimal)(rand.NextDouble() * 600);
+                    financialData.FUTMTMDay = (decimal)(rand.NextDouble() * 800);
+                    financialData.AllotedFund = (decimal)(rand.NextDouble() * 100000);
+                    financialData.UtilizedPercentage = rand.NextDouble() * 100;
+                    financialData.ExposureMargin2 = (decimal)(rand.NextDouble() * 5000);
+                }
+            }
+            BroadcastUpdatedData();
+            Thread.Sleep(1000); // Update every second
+        }
+    }
+
+
+    static void BroadcastUpdatedData()
+    {
+        string updatedData = SerializeTradingData();
+        byte[] data = Encoding.ASCII.GetBytes(updatedData);
+
+        lock (clients)
+        {
+            foreach (var clientEP in clients.Values)
+            {
+                try
+                {
+                    udpServer.Send(data, data.Length, clientEP);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error sending to client {clientEP}: {ex.Message}");
+                }
+            }
+        }
+    }
+
+    static string SerializeTradingData()
+    {
+        lock (tradingData)
+        {
+            var sb = new StringBuilder();
+            foreach (var item in tradingData)
+            {
+                sb.AppendLine($"{item.Key}:{item.Value:F2}");
+            }
+            return sb.ToString();
+        }
+    }
+}
